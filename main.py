94abe07bf6f5c12c4c24fef4ed088f58a9ea015d
Kamil Kluska <kamil6kluska7@gmail.com>
2025-07-29 21:43:02 +0200

    read list of stickers from file

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from TradeitScraper import TradeitScraper
 
 
+
 if __name__ == "__main__":
-    minPrice = 5
-    maxPrice = 10
-    stickers_to_lookup=['Holo', 'Katowice 2015', 'Cologne 2014', 'Atlanta 2017']
+    minPrice = 10
+    maxPrice = 20
+
+    with open("stickers.txt", "r") as file:
+        stickers_to_lookup = file.read().splitlines()
 
     scraper = TradeitScraper(skin_min_price=minPrice, skin_max_price=maxPrice, stickers_to_lookup=stickers_to_lookup)
     scraper.scrape()
